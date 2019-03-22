a6c62381883a8c1649bed46085b0aea114b11271
MrRikkert <hendrikx.rik@gmail.com>
2019-03-22 23:29:58 +0100

    load csv file

--- src/modules/App/App.js
+++ src/modules/App/App.js
@@ -3,15 +3,35 @@ import Navbar from "../Navbar/Navbar"
 import './App.css';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Treemap from '../Treemap/Treemap';
+import * as Papa from 'papaparse';
 
 class App extends Component {
+  state = {}
+
+  componentDidMount() {
+    let parent = this
+    let path = require("../../assets/data.csv")
+    Papa.parse(path, {
+      download: true,
+      header: true,
+      complete: function (results) {
+        parent.setState({
+          data: results.data
+        })
+      }
+    });
+  }
+
   render() {
     return (
       <Router>
         <div>
           <Navbar />
           <Switch>
-            <Route exact path="/treemap" component={Treemap} />
+            <Route
+              exact
+              path="/treemap"
+              render={(props) => <Treemap data={this.state.data} />} />
           </Switch>
         </div>
       </Router>
--- src/modules/Treemap/Treemap.js
+++ src/modules/Treemap/Treemap.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class Treemap extends Component {
-    state = {
+    componentDidMount() {
+        console.log(this.props.data)
+        this.setState({
+            data: this.props.data
+        })
     }
 
     render() {
