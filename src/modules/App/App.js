374bcbcc328707e059e75eca90633e3810305873
MrRikkert <hendrikx.rik@gmail.com>
2019-03-28 12:45:28 +0100

    added country filter

--- src/modules/App/App.js
+++ src/modules/App/App.js
@@ -6,6 +6,13 @@ import SideBar from '../Sidebar/Sidebar';
 import * as Papa from 'papaparse';
 
 class App extends Component {
+  state = {
+    fullData: []
+  }
+
+  componentDidUpdate(prevProps) {
+  }
+
   getData = (parent, transformData) => {
     let path = require("../../assets/data.csv")
     Papa.parse(path, {
@@ -26,20 +33,51 @@ class App extends Component {
         <Route
           exact
           path="/treemap"
-          render={(props) => <Treemap getData={this.getData} />} />
+          render={(props) => <Treemap
+            getData={this.getData} />} />
       </Switch>
     )
   }
 
+  getCountries = (parent) => {
+    // return this.state.fullData.map((d) => {
+    //   return {
+    //     label: d.country,
+    //     value: d.country
+    //   }
+    // })
+
+    let path = require("../../assets/data.csv")
+    Papa.parse(path, {
+      download: true,
+      header: true,
+      complete: function (results) {
+        parent.setState({
+          options: results.data
+            .filter((d) => {
+              return parseInt(d.year) === 2012
+            })
+            .map((d) => {
+              return {
+                label: d.country,
+                value: d.country,
+              }
+            })
+        })
+      }
+    });
+  }
+
   render() {
     return (
-      <Router>
+      <Router className="hallo">
         <div>
           <div className="flex-container">
             <div className="content" id="content">
               {this.renderRoutes()}
             </div>
-            <SideBar />
+            <SideBar
+              getCountries={this.getCountries} />
           </div>
         </div>
       </Router>
--- src/modules/Sidebar/Sidebar.js
+++ src/modules/Sidebar/Sidebar.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import "./Sidebar.css"
+import Select from "react-dropdown-select";
 
-class Treemap extends Component {
+class SideBar extends Component {
+    state = {
+        options: []
+    };
+
+    componentDidMount() {
+        this.setState({
+            options: this.props.getCountries(this)
+        })
+    }
 
     render() {
         return (
             <div className="sidenav">
-                hallo
+                <Select
+                    placeholder="Type to search"
+                    multi
+                    clearable
+                    onChange={() => undefined}
+                    values={[]}
+                    options={this.state.options}
+                />
             </div>
         );
     }
 }
 
-export default Treemap;
+export default SideBar;
