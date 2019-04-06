08f8c49aa7fe397e93e0d0a5e0d4deb38c2793c5
MrRikkert <hendrikx.rik@gmail.com>
2019-04-06 22:32:36 +0200

    updated graph title

--- src/modules/EmissionsAreaGraph/EmissionsAreaGraph.js
+++ src/modules/EmissionsAreaGraph/EmissionsAreaGraph.js
@@ -8,151 +8,151 @@ let yMin = 1
 let yType = "linear";
 
 class EmissionsAreaGraph extends Component {
-    state = {
-        data: [{
-            "id": "",
-            "color": "",
-            "data": [
-                {
-                    "x": 0,
-                    "y": 0
-                },
-            ]
-        }],
-        filter: {},
+  state = {
+    data: [{
+      "id": "",
+      "color": "",
+      "data": [
+        {
+          "x": 0,
+          "y": 0
+        },
+      ]
+    }],
+    filter: {},
+  }
+
+  async componentDidMount() {
+    this.setState({
+      filter: this.props.filter
+    })
+    this.transformData()
+    this.props.setTitle("CO2 emissions per country (kt)")
+  }
+
+  async componentDidUpdate(prevProps) {
+    if (this.props !== prevProps) {
+      this.setState({
+        filter: this.props.filter
+      })
+      this.transformData()
+
+      this.setYValues()
     }
+  }
 
-    async componentDidMount() {
-        this.setState({
-            filter: this.props.filter
-        })
-        this.transformData()
-        this.props.setTitle("CO2 emissions By fuel burned (kt)")
+  setYValues = async () => {
+    if (this.state.filter.log) {
+      yType = "log"
+      yMin = this.state.filter.metric.yMinLog
+      yMax = this.state.filter.metric.yMaxLog
+    } else {
+      yType = "linear"
+      yMin = "auto"
+      yMax = "auto"
     }
+  }
 
-    async componentDidUpdate(prevProps) {
-        if (this.props !== prevProps) {
-            this.setState({
-                filter: this.props.filter
-            })
-            this.transformData()
+  getLabel = (node) => {
+    return node.country + " (" + node.name + ")"
+  }
 
-            this.setYValues()
-        }
+  divide = (x, y) => {
+    if (!y) {
+      return 0
     }
+    return x / y
+  }
 
-    setYValues = async () => {
-        if (this.state.filter.log) {
-            yType = "log"
-            yMin = this.state.filter.metric.yMinLog
-            yMax = this.state.filter.metric.yMaxLog
-        } else {
-            yType = "linear"
-            yMin = "auto"
-            yMax = "auto"
-        }
+  getGroupedData = (items, key) => items.reduce(
+    (result, item) => ({
+      ...result,
+      [item[key]]: [
+        ...(result[item[key]] || []),
+        item,
+      ],
+    }),
+    {},
+  );
+
+  transformData = async () => {
+    let grouped = this.getGroupedData(this.props.getFilteredData(false), "country")
+
+    let data = [];
+    for (let country in grouped) {
+      data.push({
+        id: country,
+        data: grouped[country]
+          .map((d) => {
+            let divider = this.props.getDivider(d)
+            return {
+              x: parseInt(d.year),
+              y: this.divide(d.total_e, divider),
+            }
+          })
+          .filter((d) => {
+            return d.y > 0;
+          })
+      })
     }
+    this.setState({
+      data: data
+    })
+  }
 
-    getLabel = (node) => {
-        return node.country + " (" + node.name + ")"
-    }
+  getColor = (d) => {
+    return ColorHelper.getColorFromString(d.id)
+  }
 
-    divide = (x, y) => {
-        if (!y) {
-            return 0
-        }
-        return x / y
-    }
-
-    getGroupedData = (items, key) => items.reduce(
-        (result, item) => ({
-            ...result,
-            [item[key]]: [
-                ...(result[item[key]] || []),
-                item,
-            ],
-        }),
-        {},
+  render() {
+    return (
+      <div className="emission-line-graph">
+        <ResponsiveLine
+          curve="monotoneX"
+          data={this.state.data}
+          colors="set1"
+          colorBy={this.getColor}
+          animate={true}
+          motionStiffness={200}
+          motionDamping={20}
+          enableGridY={false}
+          margin={{
+            "top": 15,
+            "right": 10,
+            "bottom": 80,
+            "left": 90
+          }}
+          xScale={{
+            "type": "linear",
+            "min": 1970,
+            "max": 2012,
+          }}
+          yScale={{
+            "type": yType,
+            "base": 10,
+            "min": yMin,
+            "max": yMax
+          }}
+          axisBottom={{
+            "orient": "bottom",
+            "tickSize": 5,
+            "tickPadding": 5,
+            "tickRotation": 90,
+            "tickValues": 10,
+            "legend": "Year",
+            "legendOffset": 70,
+            "legendPosition": "middle"
+          }}
+          axisLeft={{
+            "orient": "left",
+            "tickSize": 5,
+            "tickPadding": 5,
+            "tickRotation": 0,
+            "tickValues": 6,
+          }} />
+      </div>
     );
-
-    transformData = async () => {
-        let grouped = this.getGroupedData(this.props.getFilteredData(false), "country")
-
-        let data = [];
-        for (let country in grouped) {
-            data.push({
-                id: country,
-                data: grouped[country]
-                    .map((d) => {
-                        let divider = this.props.getDivider(d)
-                        return {
-                            x: parseInt(d.year),
-                            y: this.divide(d.total_e, divider),
-                        }
-                    })
-                    .filter((d) => {
-                        return d.y > 0;
-                    })
-            })
-        }
-        this.setState({
-            data: data
-        })
-    }
-
-    getColor = (d) => {
-        return ColorHelper.getColorFromString(d.id)
-    }
-
-    render() {
-        return (
-            <div className="emission-line-graph">
-                <ResponsiveLine
-                    curve="monotoneX"
-                    data={this.state.data}
-                    colors="set1"
-                    colorBy={this.getColor}
-                    animate={true}
-                    motionStiffness={200}
-                    motionDamping={20}
-                    enableGridY={false}
-                    margin={{
-                        "top": 15,
-                        "right": 10,
-                        "bottom": 80,
-                        "left": 90
-                    }}
-                    xScale={{
-                        "type": "linear",
-                        "min": 1970,
-                        "max": 2012,
-                    }}
-                    yScale={{
-                        "type": yType,
-                        "base": 10,
-                        "min": yMin,
-                        "max": yMax
-                    }}
-                    axisBottom={{
-                        "orient": "bottom",
-                        "tickSize": 5,
-                        "tickPadding": 5,
-                        "tickRotation": 90,
-                        "tickValues": 10,
-                        "legend": "Year",
-                        "legendOffset": 70,
-                        "legendPosition": "middle"
-                    }}
-                    axisLeft={{
-                        "orient": "left",
-                        "tickSize": 5,
-                        "tickPadding": 5,
-                        "tickRotation": 0,
-                        "tickValues": 6,
-                    }} />
-            </div>
-        );
-    }
+  }
 }
 
 export default EmissionsAreaGraph;
