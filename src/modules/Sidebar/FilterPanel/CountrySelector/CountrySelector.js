b41ac4139b20e65fa6ecee9f8a93721b8da531bf
MrRikkert <hendrikx.rik@gmail.com>
2019-04-11 11:34:30 +0200

    improved country selector

--- src/modules/Sidebar/FilterPanel/CountrySelector/CountrySelector.js
+++ src/modules/Sidebar/FilterPanel/CountrySelector/CountrySelector.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
-import Select from 'react-select';
+import Select, { components } from 'react-select';
 import "./CountrySelector.css"
 import ColorHelper from '../../../../helpers/ColorHelper';
+
+const MultiValueContainer = (props) => {
+  return (
+    <div>
+      <span>hallo</span>
+    </div>
+  );
+};
+
+const ValueContainer = ({ children, ...props }) => (
+  <components.ValueContainer {...props}>
+    {children[0].length > 0 &&
+      <span>Selected countries: {children[0].length}</span>
+    }
+    {children}
+  </components.ValueContainer>
+);
 
 class CountrySelector extends Component {
 
@@ -14,6 +31,7 @@ class CountrySelector extends Component {
           placeholder="Type to search"
           isMulti
           clearable
+          components={{ ValueContainer }}
           onChange={(values) => this.props.updateSelectedCountries(values)}
           options={this.props.allCountries}
           value={this.props.filter.countries}
@@ -21,7 +39,9 @@ class CountrySelector extends Component {
             multiValue: (styles, { data }) => {
               return {
                 ...styles,
+                display: "none",
                 backgroundColor: data.color,
+                flexShrink: 0,
               }
             },
             multiValueLabel: (styles, { data }) => {
@@ -30,7 +50,15 @@ class CountrySelector extends Component {
                 color: ColorHelper.getFontColor(data.color)
 
               }
-            }
+            },
+            valueContainer: (styles, { data }) => {
+              return {
+                ...styles,
+                flexWrap: "hidden",
+                whiteSpace: "nowrap",
+                textOverflow: "ellipsis"
+              }
+            },
           }}
         />
       </React.Fragment>
